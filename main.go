ae7a4cf8668ff0cb8c4e5a939c0044927baac6ca
MarcoVitoC <marcovito.chr@gmail.com>
2023-09-06 22:22:15 +0700

    feat: terminate game when found specified condition

--- main.go
+++ main.go
@@ -14,30 +14,32 @@ const ROWS, COLS int = 10, 15
 var cave = make([][]Cell, ROWS)
 var posX, posY int = 1, 1
 
-var wall Cell = Cell{"🧱", ""}
-var dark Cell = Cell{"⬛", "safe"}
-var isDiscovered Cell = Cell{"  ", ""}
-var player Cell = Cell{"🙂", ""}
-var treasure Cell = Cell{"💎", ""} 
+var wall string = "🧱"
+var dark string = "⬛"
+var discovered string = "  "
+var player string = "🙂"
+var treasure string = "💎"
 
 func generateCave() {
 	for y:=0; y<ROWS; y++ {
 		cave[y] = make([]Cell, COLS)
 		for x:=0; x<COLS; x++ {
 			if y == 0 || y == ROWS - 1 || x == 0 || x == COLS - 1 {
-				cave[y][x] = wall
+				cave[y][x].symbol = wall
+				cave[y][x].status = ""
 			} else {
-				cave[y][x] = dark
+				cave[y][x].symbol = dark
+				cave[y][x].status = "safe"
 				
 				if rand.Intn(10) == 0 {
-					cave[y][x].symbol = "👾"
+					cave[y][x].status = "death"
 				}
 			}
 		}
 	}
 
-	cave[posY][posX] = player
-	cave[ROWS - 2][COLS - 2] = treasure
+	cave[posY][posX].symbol = player
+	cave[ROWS - 2][COLS - 2].symbol = treasure
 }
 
 func displayCave() {
@@ -49,45 +51,72 @@ func displayCave() {
 	}
 }
 
-func move() {
-	var move string
-
-	fmt.Print("Input move [w|a|s|d]: ")
-	fmt.Scan(&move); fmt.Scanln()
-
-	switch move {
-		case "w":
-			if cave[posY - 1][posX] != wall {
-				cave[posY][posX] = isDiscovered
-				posY--
-				cave[posY][posX] = player
-			}
-		case "a":
-			if cave[posY][posX - 1] != wall {
-				cave[posY][posX] = isDiscovered
-				posX--
-				cave[posY][posX] = player
-			}
-		case "s":
-			if cave[posY + 1][posX] != wall {
-				cave[posY][posX] = isDiscovered
-				posY++
-				cave[posY][posX] = player
-			}
-		case "d":
-			if cave[posY][posX + 1] != wall {
-				cave[posY][posX] = isDiscovered
-				posX++
-				cave[posY][posX] = player
-			}
-	}
-}
-
 func main() {
+	var move string
 	generateCave()
 
 	for {
 		displayCave()
-		move()
+		
+		fmt.Print("Input move [w|a|s|d]: ")
+		fmt.Scan(&move); fmt.Scanln()
+
+		if move == "w" {
+			if cave[posY - 1][posX].symbol != wall {
+				if cave[posY - 1][posX].status == "death" {
+					fmt.Println("You just found the death cell and lost. 💀")
+					break
+				} else if cave[posY - 1][posX].symbol == "💎" {
+					fmt.Println("Congratulations! You have won the game. 👏")
+					break
+				}
+
+				cave[posY][posX].symbol = discovered
+				posY--
+				cave[posY][posX].symbol = player
+			}
+		} else if move == "a" {
+			if cave[posY][posX - 1].symbol != wall {
+				if cave[posY][posX - 1].status == "death" {
+					fmt.Println("You just found the death cell and lost. 💀")
+					break
+				} else if cave[posY][posX - 1].symbol == "💎" {
+					fmt.Println("Congratulations! You have won the game. 👏")
+					break
+				}
+
+				cave[posY][posX].symbol = discovered
+				posX--
+				cave[posY][posX].symbol = player
+			}
+		} else if move == "s" {
+			if cave[posY + 1][posX].symbol != wall {
+				if cave[posY + 1][posX].status == "death" {
+					fmt.Println("You just found the death cell and lost. 💀")
+					break
+				} else if cave[posY + 1][posX].symbol == "💎" {
+					fmt.Println("Congratulations! You have won the game. 👏")
+					break
+				}
+
+				cave[posY][posX].symbol = discovered
+				posY++
+				cave[posY][posX].symbol = player
+			}
+		} else if move == "d" {
+			if cave[posY][posX + 1].symbol != wall {
+				if cave[posY][posX + 1].status == "death" {
+					fmt.Println("You just found the death cell and lost. 💀")
+					break
+				} else if cave[posY][posX + 1].symbol == "💎" {
+					fmt.Println("Congratulations! You have won the game. 👏")
+					break
+				}
+
+				cave[posY][posX].symbol = discovered
+				posX++
+				cave[posY][posX].symbol = player
+			}
+		}
 	}
 }
